2b23c61fd9665ad4b4980b71498fe25b74062cf3
Raghu Betina <raghu@firstdraft.com>
2018-05-10 14:27:54 +0000

    Direct association between users and questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :questions,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
