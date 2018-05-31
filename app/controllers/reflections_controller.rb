2297dc8030d8f8d249bdfbee5aa194b20dab4590
Jeff Carlson <jeff.carlson1@kellogg.northwestern.edu>
2018-05-31 04:35:47 +0000

    Improved reflection index page

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -6,7 +6,7 @@ class ReflectionsController < ApplicationController
       redirect_to("/new_reflection")
     else
     render("/reflections_prototype/setup.html.erb")
-     end
+    end
     
   end
   
@@ -34,19 +34,28 @@ class ReflectionsController < ApplicationController
     rq3.save
     
     @questions = ReflectionQuestion.where(:user_id => current_user.id)
+      
       date_to = DateTime.now
-      date_from = DateTime.now - 10
-    @responses = Response.where(:user_id => current_user.id, :created_at => date_from.beginning_of_day..date_to.end_of_day).all
-    puts @questions
-    render("reflections_prototype/new_reflection.html.erb")  
-
+      if current_user.last_reflection_on == nil
+      date_from = current_user.created_at.to_date
+      else
+      date_from = current_user.last_reflection_on
+      end
+      puts date_from
+      @responses = Response.where(:user_id => current_user.id, :created_at => date_from.beginning_of_day..date_to.end_of_day).all
+  
   end
   
   def new_reflection
       @questions = ReflectionQuestion.where(:user_id => current_user.id)
-      date_to = DateTime.now
-      date_from = DateTime.now - 10
       
+      date_to = DateTime.now
+      if current_user.last_reflection_on == nil
+      date_from = current_user.created_at.to_date
+      else
+      date_from = current_user.last_reflection_on
+      end
+      puts date_from
       @responses = Response.where(:user_id => current_user.id, :created_at => date_from.beginning_of_day..date_to.end_of_day).all
     
     render("reflections_prototype/new_reflection.html.erb")
@@ -56,11 +65,13 @@ class ReflectionsController < ApplicationController
   def submit_reflection
     r = Reflection.new
     r.user_id = current_user.id
-    r.reflection_interval = 7
-    if r.last_reflection_on == nil
-      r.last_reflection_on = current_user.created_at.to_date
+    if r.previous_reflection_date == nil
+      r.previous_reflection_date = current_user.created_at.to_date
     end
     r.save
+    
+    current_user.last_reflection_on = r.created_at.to_date
+    current_user.save(validate: false)
     
     q0 = params[:question_id_0]
     a0 = params[:answer_0]
@@ -69,23 +80,29 @@ class ReflectionsController < ApplicationController
     q2 = params[:question_id_2]
     a2 = params[:answer_2]
     
+    if a0 != nil
     answer = ReflectionAnswer.new
     answer.reflection_id = r.id
     answer.reflection_question_id = q0
     answer.reflection_answer_text = a0
     answer.save
+    end
     
+    if a1 != nil
     answer = ReflectionAnswer.new
     answer.reflection_id = r.id
     answer.reflection_question_id = q1
     answer.reflection_answer_text = a1
     answer.save
+    end
     
+    if a2 != nil
     answer = ReflectionAnswer.new
     answer.reflection_id = r.id
     answer.reflection_question_id = q2
     answer.reflection_answer_text = a2
     answer.save
+    end
     
     @reflections = Reflection.where(:user_id => current_user.id)
     render("/reflection_templates/index.html.erb")
@@ -95,7 +112,7 @@ class ReflectionsController < ApplicationController
 
   def index
     
-    @reflections = Reflection.where(:user_id => current_user.id)
+    @reflections = Reflection.where(:user_id => current_user.id).order(created_at: :desc)
     if @reflections.count == 0
       redirect_to("/new_reflection")
     else
